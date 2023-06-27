d8d2420d32657cd1fdbebca6aaba004cef11e130
AlainaMarta <alainamarta@gmail.com>
2023-06-27 16:46:39 -0300

    resolução do requisito 1

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,18 @@
 // Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  try {
+    const URL_API = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`;
+    const response = await fetch(URL_API);
+    const data = await response.json();
+    if (await data.length === 0) {
+      throw new Error('Nenhuma cidade encontrada');
+    }
+    return data;
+  } catch (error) {
+    return window.alert(error.message);
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
